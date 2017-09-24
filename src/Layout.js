1cdba1bafe076b94087317d78e4abd92dfc3b831
tyemerick <emerickt@purdue.edu>
2017-09-24 18:10:30 -0400

    Added team management componet to nav bar.

--- src/Layout.js
+++ src/Layout.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {Row, Col} from 'react-bootstrap';
 import {Route, NavLink} from 'react-router-dom';
 import Account from './Account'
-import Team from './Team';
+import Team from './Team'
+import TeamManagement from './TeamManagement';
 
 
 export default class Layout extends Component{
@@ -16,12 +17,14 @@ export default class Layout extends Component{
                         <ul className="nav flex-column nav-fill nav-pills">
                             <NavLink className="nav-link" exact to="/" activeClassName="active">Home</NavLink>
                             <NavLink className="nav-link" to="/team" activeClassName="active">Team</NavLink>
-                                <NavLink className="nav-link" to="/account" activeClassName="active">Account</NavLink>
+                            <NavLink className="nav-link" to="/team-management" activeClassName="active">Team Management</NavLink>
+                            <NavLink className="nav-link" to="/account" activeClassName="active">Account</NavLink>
                         </ul>
                     </Col>
 
                     <Col sm={8}>
                         <Route path={"/account"} component={Account}/>
+                        <Route path={"/team-management"} component={TeamManagement}/>
                         <Route path={"/team"} component={Team}/>
                     </Col>
                 </Row>
---
+++ src/TeamManagement.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class TeamManagement extends Component{
+
+    render(){
+        return <h1>Scouting Team Member Management</h1>
+    }
+}
