4812b5375fc0794aec57846f6d561bcbf9654004
Sanjeet Suhag <sanjeetsuhag@Sanjeets-MacBook-Pro.local>
2017-09-24 15:18:35 -0400

    Fixes layout of Login component forms.

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import './Login.css';
 
-export default class Login extends Component{
+export default class Login extends Component {
 
     render() {
-        return(
-            <div class="container">
-                <img class="login_logo" src="https://i.imgur.com/WawaXKU.png" alt="FRISS"/>
-                <form onSubmit={this.handleLoginSubmit}>
-                    <input type="text" placeholder="Email" />
-                    <input type="text" placeholder="Password" />
-                    <input type="Submit" value="Login" />
-                </form>
+        return (
+            <div className="container">
+                <center>
+                    <div className="image-container">
+                    <img className="login_logo" src="https://i.imgur.com/WawaXKU.png" alt="FRISS"/>
+                    </div>
+                </center>
+                <div className="form-container">
+                    <form className="form" onSubmit={this.handleLoginSubmit}>
+                        <input className="email_text" type="text" placeholder="Email"/>
+
+                        <input type="text" placeholder="Password"/>
+
+                        <center><input type="Submit" value="Login"/></center>
+                        <div className="links-container">
+                            <a href="/signup">Sign Up</a>
+
+                            <a href="/reset">Forgot Password?</a>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
