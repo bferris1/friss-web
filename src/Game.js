3658e1af5c0c84c7458b9c78c1cf72129863b40f
Unknown <emerickt@purdue.edu>
2017-10-01 19:48:56 -0400

    Added a form to the Game page.

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
+import {Button} from 'reactstrap';
+
+import NewGameForm from './NewGameForm';
 
 export default class Game extends Component{
 
+    constructor(){
+      super();
+      this.handleChange = this.handleChange.bind(this);
+      this.handleAddGame = this.handleAddGame.bind(this);
+      this.addGame = this.handleAddGame.bind(this);
+      this.state = {
+        addForm: false,
+        games: []
+      }
+    }
+
+    handleChange(e){
+        this.setState({[e.target.name]:e.target.value});
+        console.log(this.state);
+    }
+
+    handleAddGame(game){
+      let games = this.state.games;
+      games.push(game);
+      this.setState({games:games});
+      console.log(game);
+    }
+
     render(){
-        return <h1>Create Games</h1>
+
+        let newForm = null;
+        let newFormLink = <Button color="link" name={"addForm"} value={true} onClick={this.handleChange}>Add a New Game</Button>
+        if(this.state.addForm){
+          console.log('adding a new form');
+          newForm = <NewGameForm addGame={this.handleAddGame} />
+          newFormLink = null;
+        }
+
+
+        return(
+          <div className='Game'>
+            <h1>Create Games</h1>
+            {newFormLink}
+            {newForm}
+          </div>
+        )
     }
 }
