72272572175f41a3aa834bfd864ece1cf36c53b7
tyemerick <emerickt@purdue.edu>
2017-10-01 15:11:42 -0400

    Added Game page to nav bar.

---
+++ src/Game.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class Game extends Component{
+
+    render(){
+        return <h1>Create Games</h1>
+    }
+}
--- src/Layout.js
+++ src/Layout.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {Row, Col} from 'reactstrap';
 import {Route, NavLink} from 'react-router-dom';
 import Account from './Account'
+import Game from './Game'
 import Team from './Team';
 
 
@@ -16,12 +17,14 @@ export default class Layout extends Component{
                         <ul className="nav flex-column nav-fill nav-pills">
                             <NavLink className="nav-link" exact to="/" activeClassName="active">Home</NavLink>
                             <NavLink className="nav-link" to="/team" activeClassName="active">Team</NavLink>
+                            <NavLink className="nav-link" to="/game" activeClassName="active">Team</NavLink>
                             <NavLink className="nav-link" to="/account" activeClassName="active">Account</NavLink>
                         </ul>
                     </Col>
 
                     <Col sm={8}>
                         <Route path={"/account"} component={Account}/>
+                        <Route path={"/game"} component={Game}/>
                         <Route path={"/team"} component={Team}/>
                     </Col>
                 </Row>
