87430b607ba374402f258b892198cf7e77828ac9
Ben <ben.ferris1@gmail.com>
2017-10-01 16:37:45 -0400

    use new Form component on login page

--- src/Login.js
+++ src/Login.js
@@ -1,25 +1,19 @@
 import React, {Component} from 'react';
 import './Login.css';
 import {Row, Col, Button} from 'reactstrap';
-import {EmailInput, PasswordInput} from './form';
-import {AvForm} from 'availity-reactstrap-validation';
+import {EmailInput, PasswordInput, Form} from './form';
 
 export default class Login extends Component {
 
     constructor(props){
         super (props);
         this.handleLogin = this.handleLogin.bind(this);
-        this.handleChange = this.handleChange.bind(this);
         this.state = {};
 
     }
 
-    handleLogin(){
-        console.log(this.state);
-    }
-
-    handleChange(e){
-        this.setState({[e.target.name]: e.target.value});
+    handleLogin(formState){
+        console.log(formState);
     }
 
     render() {
@@ -27,15 +21,15 @@ export default class Login extends Component {
             <Row>
                 <Col sm={{size:6, offset:3}}>
                     <img className="img-fluid mx-auto d-block" src="https://i.imgur.com/WawaXKU.png" alt="FRISS"/>
-                    <AvForm>
-                        <EmailInput value={this.state.email} onChange={this.handleChange}/>
-                        <PasswordInput value={this.state.password} onChange={this.handleChange}/>
-                        <Button onClick={this.handleLogin} block={true} color="primary">Log In</Button>
+                    <Form onSubmit={this.handleLogin}>
+                        <EmailInput value={this.state.email}/>
+                        <PasswordInput value={this.state.password}/>
+                        <Button type={"submit"} block={true} color="primary">Log In</Button>
                         <div className="links-container">
                             <a href="/signup">Sign Up</a>
                             <a href="/forgot">Forgot Password?</a>
                         </div>
-                    </AvForm>
+                    </Form>
                 </Col>
             </Row>
         )
