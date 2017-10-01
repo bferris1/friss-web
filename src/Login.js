0e7ce99560c3b571d7b3b3cc073bdd0895ebd69b
Ben <ben.ferris1@gmail.com>
2017-10-01 16:25:48 -0400

    improved login page

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,43 @@
 import React, {Component} from 'react';
 import './Login.css';
+import {Row, Col, Button} from 'reactstrap';
+import {EmailInput, PasswordInput} from './form';
+import {AvForm} from 'availity-reactstrap-validation';
 
 export default class Login extends Component {
 
+    constructor(props){
+        super (props);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {};
+
+    }
+
+    handleLogin(){
+        console.log(this.state);
+    }
+
+    handleChange(e){
+        this.setState({[e.target.name]: e.target.value});
+    }
+
     render() {
         return (
-            <div className="container">
-                <center>
-                    <div className="image-container">
-                    <img className="login_logo" src="https://i.imgur.com/WawaXKU.png" alt="FRISS"/>
-                    </div>
-                </center>
-                <div className="form-container">
-                    <form className="form" onSubmit={this.handleLoginSubmit}>
-                        <input className="email_text" type="text" placeholder="Email"/>
-
-                        <input type="text" placeholder="Password"/>
-
-                        <center><input type="Submit" value="Login"/></center>
+            <Row>
+                <Col sm={{size:6, offset:3}}>
+                    <img className="img-fluid mx-auto d-block" src="https://i.imgur.com/WawaXKU.png" alt="FRISS"/>
+                    <AvForm>
+                        <EmailInput value={this.state.email} onChange={this.handleChange}/>
+                        <PasswordInput value={this.state.password} onChange={this.handleChange}/>
+                        <Button onClick={this.handleLogin} block={true} color="primary">Log In</Button>
                         <div className="links-container">
                             <a href="/signup">Sign Up</a>
-
-                            <a href="/reset">Forgot Password?</a>
+                            <a href="/forgot">Forgot Password?</a>
                         </div>
-                    </form>
-                </div>
-            </div>
+                    </AvForm>
+                </Col>
+            </Row>
         )
     }
 }
