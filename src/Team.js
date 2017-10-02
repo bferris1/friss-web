5f287ad435b448bbcd5f493c31c973bd440547a4
Sanjeet Suhag <suhagsanjeet@gmail.com>
2017-10-02 18:31:09 -0400

    Adds Team creation template to component

--- src/Team.js
+++ src/Team.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
+import {Button} from 'reactstrap';
 
-export default class Team extends Component{
+export default class Team extends Component {
 
-    render(){
-        return <h1>Team</h1>
+    constructor() {
+        super();
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleAddTeam = this.handleAddTeam.bind(this);
+        this.addTeam = this.handleAddTeam(this);
+        this.state = {
+            addTeam: false,
+            teams: []
+        }
+    }
+
+    handleChange(e) {
+        this.setState({[e.target.name] : e.target.value});
+        console.log(this.state);
+    }
+
+
+    handleAddTeam(team) {
+
+        console.log(team);
+    }
+
+    render() {
+
+        let newForm = null;
+        let newFormLink = <Button color="link" name={"addForm"} value={true} onClick={this.handleChange}>Add A New Team</Button>
+        if (this.state.addForm) {
+
+        }
+
+        return (
+            <div className="Team">
+                <h1>Create Team</h1>
+                {newFormLink}
+                {newForm}
+            </div>
+        )
     }
 }
