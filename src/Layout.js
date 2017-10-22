2db514c88ab89768ad81a79e36258aede793b0a4
Ben <ben.ferris1@gmail.com>
2017-10-22 13:46:21 -0400

    Added user name to all pages, redirect if user is not logged in

--- src/Layout.js
+++ src/Layout.js
@@ -1,31 +1,49 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Row, Col} from 'reactstrap';
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Link, Redirect} from 'react-router-dom';
 import Account from './Account'
 import Team from './Team';
+import AuthCtrl from './AuthCtrl';
 
 
 export default class Layout extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {user:null}
+    }
+
+    componentDidMount(){
+        if (AuthCtrl.isLoggedIn())
+            AuthCtrl.get('/api/account').then((res)=>{
+                if (res.success)
+                    this.setState({user:res.user});
+                console.log(res);
+            })
+    }
+
     render(){
         return(
-            <div style={{marginTop:'30px'}}>
-                <Row>
-                    <Col sm={4}>
-                        <h1>Navigation</h1>
-                        <ul className="nav flex-column nav-fill nav-pills">
-                            <NavLink className="nav-link" exact to="/" activeClassName="active">Home</NavLink>
-                            <NavLink className="nav-link" to="/team" activeClassName="active">Team</NavLink>
-                            <NavLink className="nav-link" to="/account" activeClassName="active">Account</NavLink>
-                        </ul>
-                    </Col>
+            !AuthCtrl.isLoggedIn() ? <Redirect to={"/login"}/> :
+                <div style={{marginTop:'30px'}}>
+                    <Row>
+                        <Col sm={4}>
+                            <h1>Navigation</h1>
+                            <ul className="nav flex-column nav-fill nav-pills">
+                                <NavLink className="nav-link" exact to="/" activeClassName="active">Home</NavLink>
+                                <NavLink className="nav-link" to="/team" activeClassName="active">Team</NavLink>
+                                <NavLink className="nav-link" to="/account" activeClassName="active">Account</NavLink>
+                            </ul>
+                        </Col>
 
-                    <Col sm={8}>
-                        <Route path={"/account"} component={Account}/>
-                        <Route path={"/team"} component={Team}/>
-                    </Col>
-                </Row>
-            </div>
+                        <Col sm={8}>
+                            <p><Link className={"text-right"} to={'/account'}>{this.state.user ? this.state.user.firstName + ' ' + this.state.user.lastName: "Not logged in"}</Link></p>
+                            <Route path={"/account"} component={Account}/>
+                            <Route path={"/team"} component={Team}/>
+                        </Col>
+                    </Row>
+                </div>
         )
     }
 }
