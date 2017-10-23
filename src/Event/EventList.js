06ce2bc93d3c57a42044aeb70a3b243b167e2e5a
Sanjeet Suhag <suhagsanjeet@gmail.com>
2017-10-23 11:42:24 -0400

    Adds EventList component

---
+++ src/Event/EventList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import uuid from 'uuid';
+import {Button} from 'reactstrap';
+import AddEventForm from './AddEventForm';
+import EventCardGrid from "./EventCardGrid";
+
+export default class Event extends React.Component {
+    	
+}
