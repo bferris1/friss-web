5d40fb3b6a96e05b953fa6885ac8bd719401ca89
Sanjeet Suhag <suhagsanjeet@gmail.com>
2017-10-23 20:53:25 -0400

    Adds RobotPositionForm template

---
+++ src/RobotPositionForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class RobotPositionForm extends React.Component {
+
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        
+    }
+}
