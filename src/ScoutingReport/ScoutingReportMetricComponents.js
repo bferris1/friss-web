4e4de1ef651454c124e1b0ea7aecdb290738bba9
Tyler E <emerickt@purdue.edu>
2017-11-01 17:02:23 -0400

    Changes order of + and - button on numeric stepper.

--- src/ScoutingReport/ScoutingReportMetricComponents.js
+++ src/ScoutingReport/ScoutingReportMetricComponents.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {FormGroup, Input, Label} from 'reactstrap'
+import {FormGroup, Input, Label} from 'reactstrap';
 import NumericInput from 'react-numeric-input';
-import {LabeledInput} from '../form.js'
+
+import {LabeledInput} from '../form.js';
 
 export const NumericStepper = (props) => {
     return (
-      <div>
-        <button onClick={e => {props.onChange(props.value+props.step)}}>+</button>
+      <div style={{marginTop:'15px'}}>
+        <p style={{marginBottom:'5px'}}>{props.name}</p>
+        <button onClick={e => {props.onChange(props.value-props.step)}}>-</button>
         <input className="num-step" type="number" min={props.min} max={props.max}
                       step={props.step} value={props.value}/>
-        <button onClick={e => {props.onChange(props.value-props.step)}}>-</button>
+        <button onClick={e => {props.onChange(props.value+props.step)}}>+</button>
+
       </div>
 
     )
