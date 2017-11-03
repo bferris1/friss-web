064861a68d69ec7d49455357f94eb159dd3ec574
Sanjeet Suhag <suhagsanjeet@gmail.com>
2017-11-02 21:15:54 -0400

    Adds EventCard component to Card

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Col, Card, CardBlock, CardTitle, CardText} from 'reactstrap'
 import {Link} from 'react-router-dom';
+import {LabeledInput} from "./form";
 
 export const LabeledCard = (props) => {
   return (
@@ -15,4 +16,28 @@ export const LabeledCard = (props) => {
       </Card>
     </Col>
   )
-}
+};
+
+export const EventCard = (props) => {
+
+    let games = props.games.map((game, index) => {
+        return (<option key = {index + 1} value = {game['_id']}>{game['name']}</option>);
+    });
+    games.unshift(<option key = {0}></option>);
+
+    return (
+        <Col sm={props.col_sm} style={{marginTop:'15px'}}>
+            <Card>
+                <CardBlock>
+                    <CardTitle>{props.title}</CardTitle>
+                    <CardText>{props.description}</CardText>
+
+                    <LabeledInput type={"select"} label={"Set Game"} value = {props.gameId} id={props.gameId} onChange={props.onChange}>
+                        {games}
+                    </LabeledInput>
+                    <Link className={"btn btn-block btn-primary"} to={props.link}>{props.buttonText}</Link>
+                </CardBlock>
+            </Card>
+        </Col>
+    );
+};
