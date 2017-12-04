e01309329069101220356500c4e699ff52308b1d
Ben <ben.ferris1@gmail.com>
2017-12-04 14:23:22 -0500

    fixed DataList

--- src/Analysis/Analysis.js
+++ src/Analysis/Analysis.js
@@ -16,10 +16,13 @@ export default class Analysis extends React.Component {
             eventObj: null,
             selectedMetric: null,
             selectedTeamKey: null,
+            matches: [],
+            metricValues: []
         };
 
         this.teamSelected = this.teamSelected.bind(this);
         this.metricSelected = this.metricSelected.bind(this);
+        this.updateDataList = this.updateDataList.bind(this);
     }
 
     componentDidMount() {
@@ -38,19 +41,54 @@ export default class Analysis extends React.Component {
         });
     }
 
+    updateDataList(){
+        if (!this.state.selectedTeamKey || !this.state.selectedMetric) return;
+
+        const selectedTeamKey = this.state.selectedTeamKey;
+        const selectedMetric = this.state.selectedMetric;
+        const selectedEvent = this.state.eventObj;
+
+        let matchNumbers = [];
+        let metricValues = [];
+
+        // Get all reports for selected event.
+        Auth.get('/api/event/' + selectedEvent['_id'] +'/scoutingReports').then((reportsResponse) => {
+            return reportsResponse.reports;
+        }).then((reports) => {
+            // Filter reports by selected team.
+            reports.forEach((report) => {
+                if (report.teamKey === selectedTeamKey) {
+                    matchNumbers.push(report.matchNumber);
+                    // Filter metrics by selected metric.
+                    for (let i = 0; i < report.metricData.length; i++) {
+                        if (report.metricData[i].metric === selectedMetric['_id']) {
+                            metricValues.push(report.metricData[i].metricValue);
+                        }
+                    }
+                }
+            });
+            // Update with data.
+            this.setState({
+                matches: matchNumbers,
+                metricValues: metricValues
+            });
+        });
+    }
+
     teamSelected(teamKey) {
         this.setState({
             selectedTeamKey: teamKey
-        });
+        }, this.updateDataList);
     }
 
     metricSelected(metric) {
         this.setState({
             selectedMetric: metric
-        });
+        }, this.updateDataList);
     }
 
     render() {
+
 
         let teamList, metricList, dataList, selectedView = null;
 
@@ -60,15 +98,21 @@ export default class Analysis extends React.Component {
         }
 
         if (this.state.selectedTeamKey && this.state.selectedMetric) {
-            dataList = (<DataList selectedEvent={this.state.eventObj} selectedTeamKey={this.state.selectedTeamKey} selectedMetric={this.state.selectedMetric}/>);
+            dataList = (<DataList matches={this.state.matches} metricValues={this.state.metricValues}/>);
         }
 
         return (
-            <div>
-                {teamList}
-                {metricList}
-                {selectedView}
-                {dataList}
+            <div className="row">
+                <div className="col-sm-6">
+                    {teamList}
+                    {metricList}
+                    {selectedView}
+                </div>
+                <div className="col-sm-6">
+                    <h2>Data:</h2>
+                    {dataList}
+                </div>
+
             </div>
         );
     }
--- src/Analysis/DataList.js
+++ src/Analysis/DataList.js
@@ -1,64 +1,26 @@
 import React from 'react';
-import Auth from '../AuthCtrl';
 
 export default class DataList extends React.Component {
 
     constructor(props) {
         super(props);
 
-        this.state = {
-            matches: [],
-            metricValues: []
-        };
     }
 
-    componentDidMount() {
 
-        const selectedTeamKey = this.props.selectedTeamKey;
-        const selectedMetric = this.props.selectedMetric;
-        const selectedEvent = this.props.selectedEvent;
-
-        let matchNumbers = [];
-        let metricValues = [];
-
-        // Get all reports for selected event.
-        Auth.get('/api/event/' + selectedEvent['_id'] +'/scoutingReports').then((reportsResponse) => {
-            return reportsResponse.reports;
-        }).then((reports) => {
-            // Filter reports by selected team.
-            reports.forEach((report) => {
-                if (report.teamKey === selectedTeamKey) {
-                    matchNumbers.push(report.matchNumber);
-                    // Filter metrics by selected metric.
-                    for (var i = 0; i < report.metricData.length; i++) {
-                        if (report.metricData[i].metric === selectedMetric['_id']) {
-                            metricValues.push(report.metricData[i].metricValue);
-                        }
-                    }
-                }
-            });
-            // Update with data.
-            this.setState({
-                matches: matchNumbers,
-                metricValues: metricValues
-            });
-        });
-
-
-    }
 
     render() {
 
         let metricValuesList = [];
 
-        if (this.state.metricValues.length > 0) {
+        if (this.props.metricValues.length > 0) {
 
-            for (let i = 0; i < this.state.metricValues.length; i++) {
+            for (let i = 0; i < this.props.metricValues.length; i++) {
 
                 metricValuesList[i] = (
                     <tr key={i}>
-                        <td>{this.state.matches[i]}</td>
-                        <td>{this.state.metricValues[i]}</td>
+                        <td>{this.props.matches[i]}</td>
+                        <td>{this.props.metricValues[i]}</td>
                     </tr>
                 );
             }
